c755ec84d712505db5ab3081cbb2f2e142981039
DennisDaddy <softteez95@gmail.com>
2016-08-26 23:28:50 +0300

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826201410) do
+ActiveRecord::Schema.define(version: 20160826201953) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.boolean  "status"
+    t.integer  "post_id"
+    t.integer  "visitor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["visitor_id"], name: "index_comments_on_visitor_id"
+  end
 
   create_table "moderators", force: :cascade do |t|
     t.string   "fullname"
