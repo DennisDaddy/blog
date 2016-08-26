e5733e00befb61d03e005968aba709f98bf3a1d0
DennisDaddy <softteez95@gmail.com>
2016-08-26 22:56:07 +0300

    seed database

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,2 +1,4 @@
 class Moderator < ApplicationRecord
+has_secure_password
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+moderator =Moderator.create(
+	fullname: "Kingsley Ijomah",
+	username: "Kingsley@gmail.com",
+	password: "example")
