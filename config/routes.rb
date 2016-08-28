21e20aa436ea16c20283619989d419228b91445d
DennisDaddy <softteez95@gmail.com>
2016-08-28 11:58:25 +0300

    add controller sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'sessions/new'
+  end
+
+  namespace :admin do
+    get 'sessions/create'
+  end
+
+  namespace :admin do
+    get 'sessions/destroy'
+  end
+
+  namespace :admin do
 
   resources :moderators, only: [:index, :edit, :update]
 
