3999ddb5c00bf23cf74deb2c7f9f8b9351439e00
DennisDaddy <softteez95@gmail.com>
2016-08-29 20:15:34 +0300

    add resources tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,10 @@
 Rails.application.routes.draw do
  
-  namespace :admin do
-    get 'tags/new'
-  end
-
-  namespace :admin do
-    get 'tags/create'
-  end
-
-  namespace :admin do
-    get 'tags/edit'
-  end
-
-  namespace :admin do
-    get 'tags/update'
-  end
-
-  namespace :admin do
-    get 'tags/show'
-  end
-
-  namespace :admin do
-    get 'tags/destroy'
-  end
-
  get '/login' => 'admin/sessions#new'
  get '/logout' => 'admin/sessions#destroy'
   namespace :admin do
   resources :posts
+  resources :tags, except: [:index]
   resources :sessions, only:[:new, :create, :destroy]
   resources :moderators, only: [:index, :edit, :update]
 
