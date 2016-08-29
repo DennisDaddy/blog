0180e984d56a62109f5b4b13457ab69067ab2869
DennisDaddy <softteez95@gmail.com>
2016-08-29 20:10:27 +0300

    create controller tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
  
+  namespace :admin do
+    get 'tags/new'
+  end
+
+  namespace :admin do
+    get 'tags/create'
+  end
+
+  namespace :admin do
+    get 'tags/edit'
+  end
+
+  namespace :admin do
+    get 'tags/update'
+  end
+
+  namespace :admin do
+    get 'tags/show'
+  end
+
+  namespace :admin do
+    get 'tags/destroy'
+  end
+
  get '/login' => 'admin/sessions#new'
  get '/logout' => 'admin/sessions#destroy'
   namespace :admin do
