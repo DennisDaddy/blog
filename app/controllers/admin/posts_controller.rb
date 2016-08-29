7e60141ee528ab9c869ea7e0d14532c1475c794d
DennisDaddy <softteez95@gmail.com>
2016-08-29 11:27:33 +0300

    create search field

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,11 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
+
+     if params[:search].present?
+    @posts = Post.where("title LIKE ? OR content LIKE ?","%#{params[:search]}%","%#{params[:search]}%")
+    else
     @posts = Post.all.order(id: :desc)
+  end
   end
 
   def new
