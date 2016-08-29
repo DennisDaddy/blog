89b2d2e9d89e2ed4be68285534eec8ea910ca259
DennisDaddy <softteez95@gmail.com>
2016-08-29 20:37:40 +0300

    create tags

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,8 +1,13 @@
-class Admin::TagsController < ApplicationController
+class Admin::TagsController < Admin::ApplicationController
   def new
+    @tag = Tag.new
   end
 
   def create
+    tags_params[:name].split(',').map do |n|
+      Tag.new(name:n).save
+    end
+    redirect_to new_admin_tag_url, notice: 'Tag was successfully created'
   end
 
   def edit
@@ -15,5 +20,10 @@ class Admin::TagsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+  def tags_params
+    params.require(:tag).permit(:id, :name)
+    
   end
 end
