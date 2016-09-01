84adc64c3973a25080ea80e835b945668c7f3ec6
DennisDaddy <softteez95@gmail.com>
2016-09-01 21:44:31 +0300

    refactor search

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,11 @@
 class Admin::CommentsController < Admin::ApplicationController
   def index
     if params[:search].present?
-      @comments = Comment.joins(:visitor).where("fullname LIKE ? OR message LIKE ?", "#{params[:search]}%", "#{params[:search]}%").page params[:page]
-  	@comments = Comment.where(status: to_bool(params[:status])).page params[:page]
+      @comments = Comment.matching_fullname_or_message(params[:search]).page params[:page]
+  	else
+    @comments = Comment.where(status: to_bool(params[:status])).page params[:page]
   end
- 
+ end
   def update
   	@comment = Comment.find(params[:id])
   	if @Comment.update(status: params[:status])
@@ -21,4 +22,4 @@ class Admin::CommentsController < Admin::ApplicationController
     redirect_to :back, notice: 'comment was successfully deleted'
   end
   end
-end
+
