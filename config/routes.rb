66da6791b67b6efa45cf036c77cb0d68e2c9e6aa
DennisDaddy <softteez95@gmail.com>
2016-09-02 07:52:16 +0300

    add controller admin/messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
  
+  namespace :admin do
+    get 'messages/index'
+  end
+
+  namespace :admin do
+    get 'messages/show'
+  end
+
+  namespace :admin do
+    get 'messages/update'
+  end
+
+  namespace :admin do
+    get 'messages/destroy'
+  end
+
  get '/login' => 'admin/sessions#new'
  get '/logout' => 'admin/sessions#destroy'
   namespace :admin do
