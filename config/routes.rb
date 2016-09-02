1eefd37dd8761934dfa928f9498c338099937dcb
DennisDaddy <softteez95@gmail.com>
2016-09-02 07:56:51 +0300

    finish seeding the messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
  
-  namespace :admin do
-    get 'messages/index'
-  end
-
-  namespace :admin do
-    get 'messages/show'
-  end
-
-  namespace :admin do
-    get 'messages/update'
-  end
-
-  namespace :admin do
-    get 'messages/destroy'
-  end
+  
 
  get '/login' => 'admin/sessions#new'
  get '/logout' => 'admin/sessions#destroy'
   namespace :admin do
   resources :posts
+  resources :messages, only: [:index, :show, :update, :destroy]
   resources :visitors, only: [:index, :update, :destroy]
   resources :comments, only: [:index, :update, :destroy]
   resources :tags, except: [:index]
