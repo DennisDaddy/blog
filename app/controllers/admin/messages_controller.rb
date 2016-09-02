8a4bc28aeff41531840efca523d7bc5d69ccc6b6
DennisDaddy <softteez95@gmail.com>
2016-09-02 08:24:48 +0300

    list messages and add message delete

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,13 +1,18 @@
 class Admin::MessagesController < ApplicationController
   def index
+  	@messages = Message.all.order(id: :desc).page params[:page]
   end
 
   def show
-  end
+  end 
 
   def update
   end
 
   def destroy
+  	@mesage = Message.find(params[:id])
+    @mesage.destroy
+
+    redirect_to :back, notice: 'Message was successfully deleted'
   end
 end
